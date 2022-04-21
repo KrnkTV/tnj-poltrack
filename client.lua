8716b4c0f44e895a0f93f0cebf74b23290ec7c0d
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-04-21 23:38:06 +0930

    Initial Commit

---
+++ client.lua
@@ -0,0 +1,151 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local PlayerData = QBCore.Functions.GetPlayerData() -- Just for resource restart (same as event handler)
+local onGPS = false
+local gpsProp = 0
+
+function isPolice()
+    PlayerData = QBCore.Functions.GetPlayerData()
+    if PlayerData ~= nil then
+        local isPolice = false
+        if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
+            isPolice = true
+        end
+        return isPolice
+    end
+end
+
+--Function
+local function LoadAnimDic(dict)
+    if not HasAnimDictLoaded(dict) then
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(0)
+        end
+    end
+end
+
+local function GpsToggle(toggle)
+    onGPS = toggle
+    TriggerServerEvent("tnj-poltrack:server:ToggleGPS")
+end
+
+local function toggleGpsAnimation(pState)
+	LoadAnimDic("cellphone@")
+	if pState then
+		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
+		gpsProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
+		AttachEntityToEntity(gpsProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
+	else
+		StopAnimTask(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 1.0)
+		ClearPedTasks(PlayerPedId())
+		if gpsProp ~= 0 then
+			DeleteObject(gpsProp)
+			gpsProp = 0
+		end
+	end
+end
+
+local function toggleGps(toggle)
+    gpsMenu = toggle
+    SetNuiFocus(gpsMenu, gpsMenu)
+    if gpsMenu then
+        toggleGpsAnimation(true)
+        SendNUIMessage({type = "open"})
+    else
+        toggleGpsAnimation(false)
+        SendNUIMessage({type = "close"})
+    end
+end
+
+local function IsGpsOn()
+    return onGPS
+end
+
+--Exports
+exports("IsGpsOn", IsGpsOn)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    PlayerData = {}
+    GpsToggle(false)
+end)
+
+RegisterNetEvent('tnj-poltrack:use', function()
+    toggleGps(not gpsMenu)
+end)
+
+RegisterNetEvent('tnj-poltrack:onGpsDrop', function()
+    GpsToggle(false)
+end)
+
+-- NUI
+RegisterNUICallback('escape', function(data, cb)
+    toggleGps(false)
+end)
+
+RegisterNUICallback('GPSON', function(data, cb)
+    if not onGPS then
+        onGPS = true
+        GpsToggle(true)
+    else
+        TriggerEvent("QBCore:Notify", "GPS is already on", 'error')
+    end
+end)
+
+RegisterNUICallback('GPSOFF', function(data, cb)
+    if onGPS then
+        onGPS = false
+        GpsToggle(false)
+    else
+        TriggerEvent("QBCore:Notify", "GPS Isnt on yet", 'error')
+    end
+end)
+
+--Main Thread
+CreateThread(function()
+    while true do
+        Wait(1000)
+        if LocalPlayer.state.isLoggedIn and onGPS and isPolice() then
+            QBCore.Functions.TriggerCallback('tnj-poltrack:server:GetItem', function(hasItem)
+                if not hasItem then
+                    onGPS = false
+                    GpsToggle(false)
+                end
+            end, "gps")
+        end
+    end
+end)
+
+RegisterNetEvent('tnj-poltrack:client:addBlip', function(id, pinfo)
+	if isPolice() then
+		local id = GetPlayerFromServerId(id)
+		local pedUser = GetPlayerPed(id)
+		local blip = GetBlipFromEntity(pedUser)
+
+		if not DoesBlipExist(blip) then
+			local blipPoli = AddBlipForEntity(pedUser)
+
+			SetBlipSprite(blipPoli, 1)
+            ShowHeadingIndicatorOnBlip(blipPoli, true)
+			SetBlipAsShortRange(blipPoli, false)
+			SetBlipColour(blipPoli, 1)
+			SetBlipScale(blipPoli, 1.1)
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentSubstringPlayerName(pinfo)
+            EndTextCommandSetBlipName(blipPoli)
+			-- SetBlipNameToPlayerName(blipPoli, id)
+
+			if pedUser == PlayerPedId() then
+				QBCore.Functions.Notify('Your GPS has been activated.')
+			else
+				QBCore.Functions.Notify('A police has activated a GPS.')
+			end
+		else
+			RemoveBlip(blip)
+			if pedUser == PlayerPedId() then
+				QBCore.Functions.Notify('Your GPS has been deactivated.')
+			else
+                QBCore.Functions.Notify('A police has deactivated a GPS.')
+			end
+		end
+	end
+end)
---
+++ server.lua
@@ -0,0 +1,32 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateUseableItem("gps", function(source, item)
+    TriggerClientEvent('tnj-poltrack:use', source)
+end)
+
+RegisterNetEvent('tnj-poltrack:server:ToggleGPS', function()
+    local source = source
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.PlayerData.job.name == 'police' then
+      TriggerClientEvent("tnj-poltrack:client:addBlip", -1, tonumber(source), ("[%s] %s %s"):format(Player.PlayerData.metadata.callsign, Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname))
+    end
+end)
+
+RegisterCommand('test', function(source)
+    TriggerClientEvent('tnj-poltrack:use', source)
+end)
+
+QBCore.Functions.CreateCallback('tnj-poltrack:server:GetItem', function(source, cb, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if Player ~= nil then
+        local GPS = Player.Functions.GetItemByName(item)
+        if GPS ~= nil and not Player.PlayerData.metadata["isdead"] and not Player.PlayerData.metadata["inlaststand"] then
+            cb(true)
+        else
+            cb(false)
+        end
+    else
+        cb(false)
+    end
+end)
